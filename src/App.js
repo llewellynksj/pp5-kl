b4da903cbff6299cedf797d29365449c51efba71
Kate Llewellyn <llewellyn.ksj@gmail.com>
2024-01-18 10:16:12 +0000

    Add PostList page

--- src/App.js
+++ src/App.js
@@ -7,14 +7,48 @@ import RegisterForm from "./pages/auth/RegisterForm";
 import LoginForm from "./pages/auth/LoginForm";
 import AddPostForm from "./pages/posts/AddPostForm";
 import PostPage from "./pages/posts/PostPage";
+import PostList from "./pages/posts/PostList";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
+  // console.log("Profile ID:", profile_id);
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container>
         <Switch>
-          <Route exact path="/" render={() => <h1>Homepage</h1>} />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <PostList message="No results found. Follow a user" />
+            )}
+          />
+          <Route
+            exact
+            path="/feed"
+            render={() => (
+              <PostList
+                message="No results found"
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/favourites"
+            render={() => (
+              <PostList
+                message="No results found. Show your love to a post to view here!"
+                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+              />
+            )}
+          />
+
           <Route exact path="/login" render={() => <LoginForm />} />
           <Route exact path="/register" render={() => <RegisterForm />} />
           <Route exact path="/add" render={() => <AddPostForm />} />
---
+++ src/pages/posts/PostList.js
@@ -0,0 +1,58 @@
+import { Form, Col, Row, Container } from "react-bootstrap";
+import s from "../../styles/PostList.module.css";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../services/axiosDefaults";
+import Post from "./Post";
+import Asset from "../../components/Asset";
+import NoRes from "../../assets/no_results.png";
+
+function PostList({ message, filter = "", profile_id }) {
+  const [posts, setPosts] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        setPosts(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    fetchPosts();
+  }, [filter, pathname, profile_id]);
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>Popular profiles mobile</p>
+        {hasLoaded ? (
+          <>
+            {posts.results.length ? (
+              posts.results.map((post) => (
+                <Post key={post.id} {...post} setPosts={setPosts} />
+              ))
+            ) : (
+              <Container>
+                <Asset src={NoRes} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container>
+            <Asset spinner />
+          </Container>
+        )}
+      </Col>
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        <p>Popular profiles for desktop</p>
+      </Col>
+    </Row>
+  );
+}
+export default PostList;
