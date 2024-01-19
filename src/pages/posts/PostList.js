91b08fe58f224046d21f60254f60e952bedb899d
Kate Llewellyn <llewellyn.ksj@gmail.com>
2024-01-19 18:20:39 +0000

    Add basic search bar to postlist

--- src/pages/posts/PostList.js
+++ src/pages/posts/PostList.js
@@ -6,18 +6,20 @@ import { axiosReq } from "../../services/axiosDefaults";
 import Post from "./Post";
 import Asset from "../../components/Asset";
 import NoRes from "../../assets/no_results.png";
+import { BsSearch } from "react-icons/bs";
 
 function PostList({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     console.log("Filter before API call:", filter);
 
     const fetchPosts = async () => {
       try {
-        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
         setPosts(data);
         setHasLoaded(true);
       } catch (err) {
@@ -27,12 +29,25 @@ function PostList({ message, filter = "" }) {
 
     setHasLoaded(false);
     fetchPosts();
-  }, [filter, pathname]);
+  }, [filter, query, pathname]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
+
+        <Form
+          className={s.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search"
+          />
+        </Form>
         {hasLoaded ? (
           <>
             {posts.results.length ? (
