99e3f4ec184a3ff13408c32cae89f841d6585fa1
Kate Llewellyn <llewellyn.ksj@gmail.com>
2024-01-20 14:31:29 +0000

    Add profile data context and update hottest profiles component

--- src/pages/profiles/HottestProfiles.js
+++ src/pages/profiles/HottestProfiles.js
@@ -2,34 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import { axiosReq } from "../../services/axiosDefaults";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-import s from "../../App.module.css";
 import Asset from "../../components/Asset";
 import Profile from "./Profile";
+import { useProfileData } from "../../contexts/ProfileDataContext";
 
 const HottestProfiles = ({ mobile }) => {
-  const [profileData, setProfileData] = useState({
-    userProfile: { results: [] },
-    hottestProfiles: { results: [] },
-  });
-  const { hottestProfiles } = profileData;
-  const currentUser = useCurrentUser();
-
-  useEffect(() => {
-    const handleMount = async () => {
-      try {
-        const { data } = await axiosReq.get(
-          "/profiles/?ordering=-followers_count"
-        );
-        setProfileData((prevState) => ({
-          ...prevState,
-          hottestProfiles: data,
-        }));
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    handleMount();
-  }, [currentUser]);
+  const { hottestProfiles } = useProfileData();
 
   return (
     <Container className={`${mobile && "d-lg-none text-center mb-3"}`}>
@@ -38,14 +16,14 @@ const HottestProfiles = ({ mobile }) => {
           <p>Hottest Profiles</p>
           {mobile ? (
             <div className={`d-flex justify-content-around`}>
-              {hottestProfiles.results.map((profile) => (
+              {hottestProfiles.results.slice(0, 5).map((profile) => (
                 <Profile key={profile.id} profile={profile} mobile />
               ))}
             </div>
           ) : (
-            hottestProfiles.results.map((profile) => (
-              <Profile key={profile.id} profile={profile} />
-            ))
+            hottestProfiles.results
+              .slice(0, 5)
+              .map((profile) => <Profile key={profile.id} profile={profile} />)
           )}
         </>
       ) : (
