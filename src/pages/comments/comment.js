00f350e121cce682bd9c8636afb588bbd2549efb
Kate Llewellyn <llewellyn.ksj@gmail.com>
2024-01-20 10:25:58 +0000

    Add basic structure to comment component

---
+++ src/pages/comments/comment.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import Media from "react-bootstrap/Media";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+// import { MenuDropDown } from "../../components/MenuDropDown";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { axiosRes } from "../../services/axiosDefaults";
+
+const Comment = (props) => {
+  const {
+    profile_id,
+    profile_image,
+    owner,
+    updated_at,
+    content,
+    id,
+    setPost,
+    setComments,
+  } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/comments/${id}`);
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count - 1,
+          },
+        ],
+      }));
+
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.filter((comment) => comment.id !== id),
+      }));
+    } catch (err) {}
+  };
+
+  return (
+    <>
+      <hr />
+      <Media>
+        <Link to={`/profiles/${profile_id}`}>
+          <Avatar src={profile_image} />
+        </Link>
+        <Media.Body className="align-self-center ml-2">
+          <span>{owner}</span>
+          <span>{updated_at}</span>
+          <p>{content}</p>
+          {/* {showEditForm ? (
+            <CommentEditForm
+              id={id}
+              profile_id={profile_id}
+              content={content}
+              profileImage={profile_image}
+              setComments={setComments}
+              setShowEditForm={setShowEditForm}
+            />
+          ) : (
+            <p>{content}</p>
+          )} */}
+        </Media.Body>
+        {/* {is_owner && !showEditForm && (
+          <MenuDropDown
+            handleEdit={() => setShowEditForm(true)}
+            handleDelete={handleDelete}
+          />
+        )} */}
+      </Media>
+    </>
+  );
+};
+
+export default Comment;
--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -5,6 +5,7 @@ import { axiosReq } from "../../services/axiosDefaults";
 import Post from "./Post";
 import AddCommentForm from "../comments/AddCommentForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Comment from "../comments/comment";
 
 function PostPage() {
   const { id } = useParams();
@@ -16,11 +17,12 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
-        console.log(post);
+        setComments(comments);
       } catch (err) {
         console.log(err);
       }
@@ -46,6 +48,15 @@ function PostPage() {
           ) : comments.results.length ? (
             "Comments"
           ) : null}
+          {comments.results.length ? (
+            comments.results.map((comment) => (
+              <Comment key={comment.id} {...comment} />
+            ))
+          ) : currentUser ? (
+            <span>No comments yet</span>
+          ) : (
+            <span>No comments yet. Login to comment</span>
+          )}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
