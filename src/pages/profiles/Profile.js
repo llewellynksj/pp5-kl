adcb9cbbcff1002d1aabea5019e75b2278b9af9d
Kate Llewellyn <llewellyn.ksj@gmail.com>
2024-01-20 14:13:30 +0000

    Add logic to display follow buttons in hottest profiles

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -2,9 +2,11 @@ import React from "react";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import Avatar from "../../components/Avatar";
+import s from "../../styles/Profile.module.css";
+import { Button } from "react-bootstrap";
 
 const Profile = (props) => {
-  const { profile, mobile, imageSize = 55 } = props;
+  const { profile, mobile, imageSize = 45 } = props;
   const { id, following_id, image, owner } = profile;
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
@@ -18,8 +20,18 @@ const Profile = (props) => {
           <Avatar src={image} height={imageSize} />
         </Link>
       </div>
-      <div className={`mx-2`}>
+      <div className={`mx-2 ${s.WordBreak}`}>
         <strong>{owner}</strong>
+      </div>
+      <div className={`text-right ${!mobile && "ml-auto"}`}>
+        {!mobile &&
+          currentUser &&
+          !is_owner &&
+          (following_id ? (
+            <Button onClick={() => {}}>Unfollow</Button>
+          ) : (
+            <Button onClick={() => {}}>Follow</Button>
+          ))}
       </div>
     </div>
   );
