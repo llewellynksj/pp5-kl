81dfe8e8c102a9ea343aa57a9aa7f1494c3c354c
Kate Llewellyn <llewellyn.ksj@gmail.com>
2024-01-27 13:12:11 +0000

    Amend minor styles on sidebar and divider line

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,22 +2,34 @@ import { Container } from "react-bootstrap";
 import s from "../App.module.css";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import DividerLine from "./DividerLine";
+import { BsPlusCircleFill } from "react-icons/bs";
 
 const SideBar = ({ mobile }) => {
   return (
     <Container className={`${mobile && "d-lg-none text-center mb-3"}`}>
       <p className={`${s.Heading} mb-1`}>Quick Links</p>
       <div className="d-flex flex-column pl-2">
-        <Link to="/add" style={{ textDecoration: "none" }} className="p-2">
+        <Link
+          to="/add"
+          style={{ textDecoration: "none" }}
+          className="p-2 lead d-flex"
+        >
           Add Post
+          <div className="mx-2">
+            <BsPlusCircleFill size={18} />
+          </div>
         </Link>
-        <Link to="/trending" style={{ textDecoration: "none" }} className="p-2">
+        <Link
+          to="/trending"
+          style={{ textDecoration: "none" }}
+          className="p-2 lead"
+        >
           Trending Posts
         </Link>
         <Link
           to="/favourites"
           style={{ textDecoration: "none" }}
-          className="p-2"
+          className="p-2 lead"
         >
           Favourite Posts
         </Link>
